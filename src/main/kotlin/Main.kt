0b2945736e278250267a699d8ad3e17cd101c36b
student <simiyunangila>
2023-02-17 08:44:48 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+  var school="akirachix"
+    println (charArrayOf(school[0],school [2],school[3]))
+
+  var name = "x"
+  var age = "y"
+  var stmt = "Hi, my name is $name and am $age years old."
+
+  println(stmt)
+val meal= "breakfast"
+  println(meal.length)
+
+  var message = "Rebecca"
+  var message2 = "Jane"
+  if (message.equals(message2)){
+    println("Thats  me!")}else {
+    println("I don't know who that is")}
+
+
+}
